f8b263a574b0d4e9533e6f24c92637403532762f
Ingso Maden <146644361+HonestxKatxxu@users.noreply.github.com>
2024-06-05 13:13:50 +0545

    Initial commit

---
+++ Pages/bob.java
@@ -0,0 +1,17 @@
+abstract class pop
+{
+abstract void zoll();
+public void boll()
+{
+    System.out.println(" yo yoh concrete method");
+}
+}
+class bob extends pop{
+ void zoll(){
+    System.out.println("this is zoll method");
+ }
+ public  static void main(String[]args){
+    bob coll = new bob();
+    coll.zoll();
+ }
+}
